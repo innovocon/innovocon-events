e6e4a5e70fcbca1941f51a2321607b4808cb1e36
innovocon <innovocon.events@gmail.com>
2025-01-18 08:52:09 +0530

    Update HackNex.tsx

--- src/pages/HackNex.tsx
+++ src/pages/HackNex.tsx
@@ -153,7 +153,7 @@ export function HackNex() {
   </div>
 </div>
 
-      <AnimatedSection className="py-20 bg-gray-900">
+      <AnimatedSection className={"py-20 bg-gray-900"}>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {stats.map((item, index) => (
